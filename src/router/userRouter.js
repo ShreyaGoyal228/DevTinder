ed9dfc26475e4763ad2bdd54e2fe8aecd47b14f9
Shreya Goyal <goyalshreya228@gmail.com>
2025-08-13 11:58:44 +0530

    add : connections api

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -2,10 +2,40 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const userRouter = express.Router();
+const ALLOWED_FIELDS = [
+  "firstName",
+  "lastName",
+  "gender",
+  "age",
+  "about",
+  "skills",
+  "photoUrl",
+];
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    //Connections can be there : user can send the request to someone and can also receive it from another
+    const connections = await ConnectionRequest.find({
+      $or: [
+        { status: "accepted", fromUserId: loggedInUser._id },
+        { status: "accepted", toUserId: loggedInUser._id },
+      ],
+    })
+      .populate("fromUserId", ALLOWED_FIELDS)
+      .populate("toUserId", ALLOWED_FIELDS);
 
-// userRouter.get("/user/connections",async(req,res)=>{
-
-// })
+    //we need to find out exactly
+    const data = connections.map((row) => {
+      if (row.fromUserId._id.equals(loggedInUser._id)) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+    res.send({ message: "Connections are :", data: data });
+  } catch (err) {
+    res.status(400).send("Error :" + err.message);
+  }
+});
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
@@ -13,15 +43,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     const requestsReceived = await ConnectionRequest.find({
       status: "interested",
       toUserId: loggedInUser._id,
-    }).populate("fromUserId", [
-      "firstName",
-      "lastName",
-      "gender",
-      "age",
-      "about",
-      "skills",
-      "photoUrl",
-    ]);
+    }).populate("fromUserId", ALLOWED_FIELDS);
     res.send({ message: "Received requests: ", data: requestsReceived });
   } catch (err) {
     res.status(400).send("Error : " + err.message);
